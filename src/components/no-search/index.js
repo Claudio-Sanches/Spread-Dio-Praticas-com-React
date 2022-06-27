72aaff24423b5a670cc310a0889d9dcdd59eb7eb
Claudio-Sanches <claudiobuzzolini@gmail.com>
2022-06-27 18:54:01 -0300

    Laboratorio desenvolvido durante Bootcamp Spread Dio - Front totalmente componitizado na prática com ReactJC

---
+++ src/components/no-search/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./styled";
+
+const NoSearch = () => {
+    return ( <
+        S.Wrapper >
+        <
+        h1 > Nenhum usuário do Github pesquisado... < /h1> < /
+        S.Wrapper >
+    );
+};
+
+export default NoSearch;
